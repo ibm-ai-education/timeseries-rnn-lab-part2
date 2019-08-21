cb927fb92e4aa501d588ac365d15359db753f69e
David Carew <carew@us.ibm.com>
2019-08-21 16:15:58 -0500

    Manually recording experiment metrics as WML sample class  not genertaing metrics properly anymore

--- model-source/oilprice_rnn.py
+++ model-source/oilprice_rnn.py
@@ -152,14 +152,15 @@ if __name__ == '__main__':
         with open(config_file, 'r') as f:
             json_obj = json.load(f)
             prev_periods = int(json_obj["prev_periods"])
-            #dropout_rate = float(json_obj["dropout_rate"])
+            dropout_rate = float(json_obj["dropout_rate"])
     else:
         prev_periods = 1
-        #dropout_rate = 0.2
+        dropout_rate = 0.2
 
-    dropout_rate = 0.2
-    # data_url = "https://ibm.box.com/shared/static/ojkntksc9rdbrj52yzkqfhbc1c9kv833.csv"
 
+   ###############################################################################
+   #  Read in data
+   ###############################################################################
     data = pd.read_csv(input_data_path, index_col='DATE')
 
     # Create a scaled version of the data with oil prices normalized between 0 and 1
@@ -186,7 +187,7 @@ if __name__ == '__main__':
 
     # Build model
     lstm_units = 1000
-    epochs = 5
+    epochs = 20
     batch_size = 32
     model = Sequential()
     model.add(LSTM(lstm_units, input_shape=(trainX.shape[1], trainX.shape[2])))
@@ -206,18 +207,22 @@ if __name__ == '__main__':
 
     #print("Metrics: " + str(history.history.keys()))
     #print("Training history:" + str(history.history))
+    ###############################################################################
+    # Generate validation  metrics
+    ###############################################################################
     metrics_out = []
     for i in range(epochs):
         each_epoch = {"steps": i}
         for metric in list(history.history.keys()):
-            each_epoch[metric] = history.history[metric][i]
+            if metric.find("val") == 0:
+               each_epoch[metric] = history.history[metric][i]
         metrics_out.append(each_epoch)
 
     if environ.get("RESULT_DIR") is not None:
         metrics_file = os.environ["RESULT_DIR"] + '/val_dict_list.json'
     else:
         metrics_file = 'val_dict_list.json'
-    #with open(), 'w') as f:
+    
     with open(metrics_file, 'w') as f:
        json.dump(metrics_out, f)
 
