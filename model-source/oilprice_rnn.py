96f995eacccc76f939b70957c0aef86d7795e102
David Carew <carew@us.ibm.com>
2019-08-21 00:08:10 -0500

    Simplified experiment for faster completion

--- model-source/oilprice_rnn.py
+++ model-source/oilprice_rnn.py
@@ -15,56 +15,10 @@ import numpy as np
 
 
 
-###############################################################################
-# Set up working directories for data, model and logs.
-###############################################################################
-
 model_filename = "oilprice_rnn.h5"
 data_filename = "WCOILWTICO.csv"
 
-# writing the train model and getting input data
-if environ.get('DATA_DIR') is not None:
-    input_data_folder = environ.get('DATA_DIR')
-    input_data_path = os.path.join(input_data_folder, data_filename)
-else:
-     input_data_path= data_filename
 
-if environ.get('RESULT_DIR') is not None:
-    output_model_folder = os.path.join(os.environ["RESULT_DIR"], "model")
-    output_model_path = os.path.join(output_model_folder, model_filename)
-else:
-    output_model_folder = "model"
-    output_model_path = os.path.join("model", model_filename)
-
-os.makedirs(output_model_folder, exist_ok=True)
-
-#writing metrics
-if environ.get('JOB_STATE_DIR') is not None:
-    tb_directory = os.path.join(os.environ["JOB_STATE_DIR"], "logs", "tb", "test")
-else:
-    tb_directory = os.path.join("logs", "tb", "test")
-
-os.makedirs(tb_directory, exist_ok=True)
-
-tensorboard = TensorBoard(log_dir=tb_directory)
-
-###############################################################################
-
-
-###############################################################################
-# Set up HPO.
-###############################################################################
-
-config_file = "config.json"
-
-if os.path.exists(config_file):
-    with open(config_file, 'r') as f:
-        json_obj = json.load(f)
-        prev_periods = int(json_obj["prev_periods"])
-        dropout_rate = float(json_obj["dropout_rate"])
-else:
-    prev_periods = 1
-    dropout_rate = 0.2
 
 def getCurrentSubID():
     if "SUBID" in os.environ:
@@ -104,58 +58,107 @@ class HPOMetrics(keras.callbacks.Callback):
 
 ###############################################################################
 
+if __name__ == '__main__':
 
-# data_url = "https://ibm.box.com/shared/static/ojkntksc9rdbrj52yzkqfhbc1c9kv833.csv"
+    ###############################################################################
+    # Set up working directories for data, model and logs.
+    ###############################################################################
+    # writing the train model and getting input data
+    if environ.get('DATA_DIR') is not None:
+        input_data_folder = environ.get('DATA_DIR')
+        input_data_path = os.path.join(input_data_folder, data_filename)
+    else:
+         input_data_path= data_filename
 
-data = pd.read_csv(input_data_path, index_col='DATE')
+    if environ.get('RESULT_DIR') is not None:
+        output_model_folder = os.path.join(os.environ["RESULT_DIR"], "model")
+        output_model_path = os.path.join(output_model_folder, model_filename)
+    else:
+        output_model_folder = "model"
+        output_model_path = os.path.join("model", model_filename)
 
-# Create a scaled version of the data with oil prices normalized between 0 and 1
-values = data['WCOILWTICO'].values.reshape(-1,1)
-values = values.astype('float32')
-#scaler = MinMaxScaler(feature_range=(0, 1))
-#scaled = scaler.fit_transform(values)
-# turn off scaler to simplify running model on future data
-scaled = values
+    os.makedirs(output_model_folder, exist_ok=True)
 
-# Split the data between training and testing
-# The first 70% of the data is used for training while the remaining 30% is used for validation
-train_size = int(len(scaled) * 0.7)
-test_size = len(scaled) - train_size
-train, test = scaled[0:train_size,:], scaled[train_size:len(scaled),:]
+    #writing metrics
+    if environ.get('JOB_STATE_DIR') is not None:
+        tb_directory = os.path.join(os.environ["JOB_STATE_DIR"], "logs", "tb", "test")
+    else:
+        tb_directory = os.path.join("logs", "tb", "test")
 
-# Generate testing and validation data
-trainX, trainY = gen_datasets(train, prev_periods)
-testX, testY = gen_datasets(test, prev_periods)
+    os.makedirs(tb_directory, exist_ok=True)
 
-# Reshape into a numpy arraya of shape (m, 1, prev_periods) where m is the number of training or testing values
-trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+    tensorboard = TensorBoard(log_dir=tb_directory)
 
-# Build model
-lstm_units = 1000
-epochs = 50
-batch_size = 32
-model = Sequential()
-model.add(LSTM(lstm_units, input_shape=(trainX.shape[1], trainX.shape[2])))
-if dropout_rate > 0.0:
-   model.add(Dropout(dropout_rate))
-model.add(Dense(1))
+    ###############################################################################
 
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
+    ###############################################################################
+    # Set up HPO.
+    ###############################################################################
 
-hpo = HPOMetrics()
+    config_file = "config.json"
 
-history = model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, validation_data=(testX, testY),  callbacks=[tensorboard, hpo], shuffle=False)
+    if os.path.exists(config_file):
+        with open(config_file, 'r') as f:
+            json_obj = json.load(f)
+            prev_periods = int(json_obj["prev_periods"])
+            dropout_rate = float(json_obj["dropout_rate"])
+    else:
+        prev_periods = 1
+        dropout_rate = 0.2
 
-hpo.close()
+    # data_url = "https://ibm.box.com/shared/static/ojkntksc9rdbrj52yzkqfhbc1c9kv833.csv"
 
-print("Training history:" + str(history.history))
+    data = pd.read_csv(input_data_path, index_col='DATE')
 
-# Check out MSE, RMSE, MAE for  testing data
-testing_error = model.evaluate(testX, testY, verbose=0)
-print('Testing error: %.5f MSE (%.5f RMSE) %.5f MAE' % (testing_error[0], sqrt(testing_error[0]), testing_error[1]))
+    # Create a scaled version of the data with oil prices normalized between 0 and 1
+    values = data['WCOILWTICO'].values.reshape(-1,1)
+    values = values.astype('float32')
+    #scaler = MinMaxScaler(feature_range=(0, 1))
+    #scaled = scaler.fit_transform(values)
+    # turn off scaler to simplify running model on future data
+    scaled = values
+
+    # Split the data between training and testing
+    # The first 70% of the data is used for training while the remaining 30% is used for validation
+    train_size = int(len(scaled) * 0.7)
+    test_size = len(scaled) - train_size
+    train, test = scaled[0:train_size,:], scaled[train_size:len(scaled),:]
+
+    # Generate testing and validation data
+    trainX, trainY = gen_datasets(train, prev_periods)
+    testX, testY = gen_datasets(test, prev_periods)
+
+    # Reshape into a numpy arraya of shape (m, 1, prev_periods) where m is the number of training or testing values
+    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+    # Build model
+    lstm_units = 1000
+    epochs = 20
+    batch_size = 32
+    model = Sequential()
+    model.add(LSTM(lstm_units, input_shape=(trainX.shape[1], trainX.shape[2])))
+    if dropout_rate > 0.0:
+       model.add(Dropout(dropout_rate))
+    model.add(Dense(1))
 
 
-# save the model
-model.save(output_model_path)
+    model.compile(loss='mae', optimizer='adam', metrics=['mae'])
+
+    hpo = HPOMetrics()
+
+    history = model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, validation_data=(testX, testY),  callbacks=[tensorboard, hpo], shuffle=False)
+
+    hpo.close()
+    #history = model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, validation_data=(testX, testY),  callbacks=[tensorboard], shuffle=False)
+
+    print("Training history:" + str(history.history))
+
+    # Check out MSE, RMSE, MAE for  testing data
+    testing_error = model.evaluate(testX, testY, verbose=0)
+    print('Testing error: %.5f MSE (%.5f RMSE) %.5f MAE' % (testing_error[0], sqrt(testing_error[0]), testing_error[1]))
+
+
+    # save the model
+    model.save(output_model_path)
